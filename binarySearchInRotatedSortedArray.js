9ca0cd0d47911c3f81447bfa819af041470b3020
sohail-27 <sohailkhan51589@gmail.com>
2025-09-11 11:56:42 +0530

    Binary Search in Rotated Sorted Array

--- binarySearchInRotatedSortedArray.js
+++ binarySearchInRotatedSortedArray.js
@@ -9,13 +9,23 @@ var binarySearchInRotatedSortedArray = function (nums, target) {
 
         if(target == nums[mid]) return mid;
 
-        if ( target > nums[mid]){
+        if ( nums[left] <= nums[mid]){
+            if(target >= nums[left] && target <nums[mid]){
+                right = mid - 1;
+            }
+            else {
             left = mid + 1;
+            }
         }
-
         else {
-            right = mid - 1
+            if(target > nums[mid] && target <= nums[right]){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1
+            }
         }
+        
 
     }
     return -1;

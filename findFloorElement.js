2246ca3fe7266c852fbcd6c49aa42a00ef6633f6
sohail-27 <sohailkhan51589@gmail.com>
2025-10-01 09:28:29 +0530

    Find Floor Element Less thand or equal to target

--- findFloorElement.js
+++ findFloorElement.js
@@ -1,22 +1,22 @@
 function findFloorElement(arr, key) {
-    
-    if(!arr || arr.length === 0) return -1;
+
+    if (!arr || arr.length === 0) return -1;
 
     let s = 0
     let e = arr.length - 1;
 
-    while(s<=e){
-         let mid = Math.floor(s+(e-s)/2);
+    while (s <= e) {
+        let mid = Math.floor(s + (e - s) / 2);
 
-                 if(target == arr[mid]) return mid;
+        if (target == arr[mid]) return mid;
 
 
-         if(key < arr[mid]){
+        if (key < arr[mid]) {
             e = mid - 1;
-         }
-         else {
+        }
+        else {
             s = mid + 1;
-         }
+        }
     }
 
     return e < 0 ? -1 : e;
@@ -24,12 +24,12 @@ function findFloorElement(arr, key) {
 }
 
 
-console.log(findFloorElement([2,3,5,9,14,16,18], 15)); //4
-console.log(findFloorElement([4,6,10], 6)); //1
-console.log(findFloorElement([1,3,8,10,15], 12));
-console.log(findFloorElement([4,6,10], 17)); //-1
-console.log(findFloorElement([4,6,10], -1)); //0
-console.log(findFloorElement([1,3,8,10,15], 12)); //3
-console.log(findFloorElement([4,6,10], 17)); //2
-console.log(findFloorElement([4,6,10], -1)); //-1
-console.log(findFloorElement([1,3,8,10,15], 12)); //3
+console.log(findFloorElement([2, 3, 5, 9, 14, 16, 18], 15)); //4
+console.log(findFloorElement([4, 6, 10], 6)); //1
+console.log(findFloorElement([1, 3, 8, 10, 15], 12));
+console.log(findFloorElement([4, 6, 10], 17)); //-1
+console.log(findFloorElement([4, 6, 10], -1)); //0
+console.log(findFloorElement([1, 3, 8, 10, 15], 12)); //3
+console.log(findFloorElement([4, 6, 10], 17)); //2
+console.log(findFloorElement([4, 6, 10], -1)); //-1
+console.log(findFloorElement([1, 3, 8, 10, 15], 12)); //3

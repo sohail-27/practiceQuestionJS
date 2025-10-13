51b548685e948097d468808c2f14a05cf1fb0368
sohail-27 <sohailkhan51589@gmail.com>
2025-10-13 09:10:49 +0530

    33. Search in Roated Sorted Array

--- 33SearchinRotatedSortedArrayBinarySearch.js
+++ 33SearchinRotatedSortedArrayBinarySearch.js
@@ -1,43 +1,43 @@
-var search = function (nums, target) {
+var search = function(nums, target) {
 
-    // Function to find the pivot element in the rotated sorted array
-    // Pivot element is the largest element in the array
-    // After pivot element, the array is rotated
-    // For example, in [4,5,6,7,0,1,2], pivot is 7
-
-    function findPivot(nums) {
+    function findPivot(nums){
         let s = 0, e = nums.length - 1;
-        if (nums[s] < nums[e]) return e; // Not rotated
-        while (s < e) {
-            let mid = Math.floor(s + (e - s) / 2);
-            if (nums[mid] > nums[mid + 1]) {
+
+        while(s<e){
+
+            let mid = Math.floor( s + ( e - s )/2);
+
+            if(mid < e && nums[mid] > nums[mid + 1]){
                 return mid;
             }
-            if (nums[mid] < nums[mid - 1]) {
-                return mid - 1;
+            if(mid > s && nums[mid] < nums[mid - 1]){
+                return mid-1;
             }
-            if (nums[mid] <= nums[s]) {
+            if(nums[mid] <= nums[s]){
                 e = mid - 1;
-            } else {
+            }
+            else{
                 s = mid + 1;
             }
         }
-        return s;
+
+        return -1
     }
+    
+    
+    function findTarget(nums, target, s, e){
+        
+        while(s<=e){
 
-    function findTarget(nums, target, s, e) {
+            let mid = Math.floor( s + ( e - s )/2);
 
-        while (s <= e) {
-
-            let mid = Math.floor(s + (e - s) / 2);
-
-            if (target < nums[mid]) { // If target is less than mid, then search in left side
-                e = mid - 1; // Move the end pointer to mid - 1
+            if(target < nums[mid]){
+                e = mid - 1;
             }
-            else if (target > nums[mid]) {// If target is greater than mid, then search in right side
-                s = mid + 1;// Move the start pointer to mid + 1
+            else if(target > nums[mid]){
+                s = mid + 1;
             }
-            else {
+            else{
                 return mid;
             }
 
@@ -45,17 +45,12 @@ var search = function (nums, target) {
         return -1;
     }
     let pivot = findPivot(nums);
-    if (target == nums[pivot]) return pivot;
-    if (target >= nums[0]) return findTarget(nums, target, 0, pivot - 1)
-    return findTarget(nums, target, pivot + 1, nums.length - 1)
+    
+    if(pivot == -1) return findTarget(nums, target, 0, nums.length-1);
+    if(target == nums[pivot]) return pivot;
+    if(target >= nums[0]) return findTarget(nums, target, 0,pivot-1) 
+    return findTarget(nums, target, pivot+1,nums.length-1) 
 
 };
-
-
-console.log(search([4, 5, 6, 7, 0, 1, 2], 0));//4
-console.log(search([4, 5, 6, 7, 0, 1, 2], 3));//-1
-console.log(search([1], 0));//-1
-console.log(search([1, 3], 3));//1
-console.log(search([3, 1], 1));//1
-console.log(search([5, 1, 3], 3));//2
-console.log(search([5, 1, 3], 5));//0
+console.log(search([4,5,6,7,0,1,2], 2));
+console.log(search([5,1,3], 5));

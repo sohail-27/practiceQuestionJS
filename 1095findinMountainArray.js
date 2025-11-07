71236b35a8a4cbb2fd165cbbf54cfedb55a37e22
sohail-27 <sohailkhan51589@gmail.com>
2025-11-08 00:04:48 +0530

    1095. Find in Mountain Array

--- 1095findinMountainArray.js
+++ 1095findinMountainArray.js
@@ -65,3 +65,4 @@ console.log(findTargetInMountainArr([0, 10, 5, 2], 5)); //2
 console.log(findTargetInMountainArr([3, 5, 3, 2, 0], 0)); //4
 console.log(findTargetInMountainArr([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1], 3)); //2
 console.log(findTargetInMountainArr([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1], 11)); //-1
+console.log(findTargetInMountainArr([0, 1, 2, 3, 4, 5, 3, 1], 5)); //5

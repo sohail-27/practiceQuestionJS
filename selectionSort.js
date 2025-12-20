8476d9ef05d1a404aa62096d3715ad7e05d5d44d
sohail-27 <sohailkhan51589@gmail.com>
2025-12-20 23:41:44 +0530

    Selection Sort

--- selectionSort.js
+++ selectionSort.js
@@ -27,5 +27,4 @@ var selectionSort = function(nums){
     return nums
 }
 console.log(selectionSort([3,1,8,0,5,4,2]))// [0,1,2,3,4,5,8]
-console.log(selectionSort([10,9,8,7,6,5,4,3,2,1]))// [1,2,3,4,5,6,7,8,9,10]
 

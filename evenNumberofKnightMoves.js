4d6b95fba10d94d78e432ac08fd52cbc5eb0a9a3
sohail-27 <sohailkhan51589@gmail.com>
2026-07-20 23:36:51 +0530

    Even Number of Knight Moves

--- evenNumberofKnightMoves.js
+++ evenNumberofKnightMoves.js
@@ -10,3 +10,4 @@ console.log(canReach([0, 0], [1, 1])); // false
 console.log(canReach([2, 3], [4, 5])); // true
 console.log(canReach([1, 1], [2, 2])); // false
 console.log(canReach([0, 0], [2, 1])); // true
+console.log(canReach([0, 0], [3, 3])); // false

c1756434a27376ec35bfeeebe27d065217bd6333
sohail-27 <sohailkhan51589@gmail.com>
2026-08-15 00:17:03 +0530

    Selection Sort

--- selectionSort.js
+++ selectionSort.js
@@ -29,3 +29,4 @@ var selectionSort = function (nums) {
 console.log(selectionSort([3, 1, 8, 0, 5, 4, 2]))// [0,1,2,3,4,5,8]
 console.log(selectionSort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]))// [1,2,3,4,5,6,7,8,9,10]
 console.log(selectionSort([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))// [1,2,3,4,5,6,7,8,9,10]
+console.log(selectionSort([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0]))// [0,1,2,3,4,5,6,7,8,9,10]

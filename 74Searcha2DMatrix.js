c0d89912f4055d4e2bbd041cf42e36b0cb5979fc
sohail-27 <sohailkhan51589@gmail.com>
2026-09-12 23:51:09 +0530

    74. Search a 2D Matrix

--- 74Searcha2DMatrix.js
+++ 74Searcha2DMatrix.js
@@ -23,5 +23,4 @@ var searchMatrix = function(matrix, target) {
 };
 
 console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3)); // true
-console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3)); // true
 
